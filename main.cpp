7910d692b3a278a08b021564156c25d4348b880f
Gavin Fernandes <gavinfernandes2012@gmail.com>
2016-01-04 23:06:34 +0800

    Update main.cpp
    Major Changes

--- main.cpp
+++ main.cpp
@@ -3,116 +3,25 @@
 #include <iostream>
 #include <sstream>
 #include "GPIOClass.h"
+#include <string>
 
-using namespace std;
-
-GPIOClass::GPIOClass()
-{
-    this->gpionum = "4"; //GPIO4 is default
-}
-
-GPIOClass::GPIOClass(string gnum)
-{
-    this->gpionum = gnum;  //Instatiate GPIOClass object for GPIO pin number "gnum"
-}
-
-int GPIOClass::export_gpio()
-{
-    string export_str = "/sys/class/gpio/export";
-    ofstream exportgpio(export_str.c_str()); // Open "export" file. Convert C++ string to C string. Required for all Linux pathnames
-    if (exportgpio < 0){
-        cout << " OPERATION FAILED: Unable to export GPIO"<< this->gpionum <<" ."<< endl;
-        return -1;
-    }
-
-    exportgpio << this->gpionum ; //write GPIO number to export
-    exportgpio.close(); //close export file
-    return 0;
-}
-
-int GPIOClass::unexport_gpio()
-{
-    string unexport_str = "/sys/class/gpio/unexport";
-    ofstream unexportgpio(unexport_str.c_str()); //Open unexport file
-    if (unexportgpio < 0){
-        cout << " OPERATION FAILED: Unable to unexport GPIO"<< this->gpionum <<" ."<< endl;
-        return -1;
-    }
-
-    unexportgpio << this->gpionum ; //write GPIO number to unexport
-    unexportgpio.close(); //close unexport file
-    return 0;
-}
-
-int GPIOClass::setdir_gpio(string dir)
-{
-
-    string setdir_str ="/sys/class/gpio/gpio" + this->gpionum + "/direction";
-    ofstream setdirgpio(setdir_str.c_str()); // open direction file for gpio
-        if (setdirgpio < 0){
-            cout << " OPERATION FAILED: Unable to set direction of GPIO"<< this->gpionum <<" ."<< endl;
-            return -1;
-        }
-
-        setdirgpio << dir; //write direction to direction file
-        setdirgpio.close(); // close direction file
-        return 0;
-}
-
-int GPIOClass::setval_gpio(string val)
-{
-
-    string setval_str = "/sys/class/gpio/gpio" + this->gpionum + "/value";
-    ofstream setvalgpio(setval_str.c_str()); // open value file for gpio
-        if (setvalgpio < 0){
-            cout << " OPERATION FAILED: Unable to set the value of GPIO"<< this->gpionum <<" ."<< endl;
-            return -1;
-        }
-
-        setvalgpio << val ;//write value to value file
-        setvalgpio.close();// close value file
-        return 0;
-}
-
-int GPIOClass::getval_gpio(string& val){
-
-    string getval_str = "/sys/class/gpio/gpio" + this->gpionum + "/value";
-    ifstream getvalgpio(getval_str.c_str());// open value file for gpio
-    if (getvalgpio < 0){
-        cout << " OPERATION FAILED: Unable to get value of GPIO"<< this->gpionum <<" ."<< endl;
-        return -1;
-            }
-
-    getvalgpio >> val ;  //read gpio value
-
-    if(val != "0")
-        val = "1";
-    else
-        val = "0";
-
-    getvalgpio.close(); //close the value file
-    return 0;
-}
-
-string GPIOClass::get_gpionum(){
-
-return this->gpionum;
-
-}
-
+using namespace::std;
 
 int main() {
 
-using namespace::std
-
-bool A=0;
-bool B=0;
-bool C=0;
-int x=0
-int y=0
+string A="0";
+string B="0";
+string C="0";
+int x=0;
+int y=0;
+int c=1;
 GPIOClass* gpio4 = new GPIOClass("4");
 GPIOClass* gpio17 = new GPIOClass("17");
-GPIOClass* gpio17 = new GPIOClass("27");
+GPIOClass* gpio27 = new GPIOClass("27");
+
+gpio4->export_gpio();
+gpio17->export_gpio();
+gpio27->export_gpio();
 
 cout << " GPIO pins exported" << endl;
 
@@ -124,22 +33,22 @@ cout << " Set GPIO pin directions" << endl;
 
 while(1)
 {
-    gpio4->getval(A);
-    gpio17->getval(B);
-    gpio27->getval(C);
-    int choice=0
+    gpio4->getval_gpio(A);
+    gpio17->getval_gpio(B);
+    gpio27->getval_gpio(C);
+    string choice= "0";
     
-    if (A == 1);
-    then choice=1
-    else if (B == 1);
-    then choice=2;
-    else if (C == 1);
-    then choice=3;
-    switch (x);
-      case 1;
-      cout << "A wave that consists of two perpendicular transverse waves with one component of the wave being a vibrating electric field and the other being a corresponding magnetic field is called a(n) \n induction wave. \n sound wave. \n electromagnetic wave. \n" ;
-      y=y+1;
-      switch (choice);
-        case 1;
-        
+    if (A == "1") 
+	{
+    choice="A" ;
+	}
+    else if (B == "1") 
+	{
+    choice="B";
+	}
+    else if (C == "1") 
+	{
+    choice="C";
+	}
+}
 }
